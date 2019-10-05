f0db0c8fdd3589d910559c17564bd4700d1fa536
jonaspohren <jonaspohren@gmail.com>
2019-10-05 09:09:38 -0300

    Deleted jwt

--- jwt/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  jwt: {
-    secret: process.env.JWT_SECRET || 'secret'
-  },
-  mongodb: {
-    uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1/jwt_db'
-  }
-};
--- jwt/db.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-const config = require('./config');
-
-mongoose.connect(config.mongodb.uri, { useNewUrlParser: true });
